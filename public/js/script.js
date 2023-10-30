4cc661839295484ea14acc3b495eabf3f9ae8b37
Vinicius Nishida <viniciusnishida13@gmail.com>
2023-10-30 19:04:20 -0300

    Adicionado funcao para mostrar exemplo

--- public/js/script.js
+++ public/js/script.js
@@ -13,5 +13,21 @@ function comecarGame(){
     setTimeout(function(){
         blocoExplicacao.style.opacity = 1;
     }, 1000);
+}
+
+function mostrarExemplos(){
+    const blocoExplicacao = document.getElementById('explicacao');
+    const blocoExemplos = document.getElementById('exemplos');
+
+    blocoExplicacao.style.opacity = 0;
+    blocoExplicacao.style.transition = 'opacity 1s';
+    setTimeout(function(){
+        blocoExplicacao.style.display = 'none';
+    }, 1000);
     
+
+    blocoExemplos.style.display = 'flex';
+    setTimeout(function(){
+        blocoExemplos.style.opacity = 1;
+    }, 1000);
 }
